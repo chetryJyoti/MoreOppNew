57ad08b37165747477c90c69de2951dec7e39732
chetryJyoti <jyotichetry087@gmail.com>
2023-05-04 15:51:31 +0530

    added some designs and api calls

---
+++ src/components/popup/Popup.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Popup = (props) => {
+    const {
+        title,
+        onCancel,
+        onSave,
+        onChange,
+        inputValue,
+    } = props;
+
+    return (
+        <div className="absolute bottom-50 left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 w-1/2 p-4 bg-white border border-gray-300 rounded-md shadow-lg">
+            <h2 className="text-lg font-bold mb-2">{title}</h2>
+            <div className="mb-4">
+                <form className=' w-full border-solid border-2 border-black p-1'>
+                    <input className=' w-full border-none outline-none' onChange={onChange} value={inputValue} />
+                </form>
+            </div>
+            <div className="flex justify-end gap-1">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={onCancel}>
+                    Cancel
+                </button>
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={onSave}>
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Popup
