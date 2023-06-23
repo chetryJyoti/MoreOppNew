0b43c1ede14e36b95e53d17e3a06dd0a07542cce
chetryJyoti <jyotichetry087@gmail.com>
2023-06-23 15:49:38 +0530

    bug fixes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="mt-3">
         <h3 className="text-xl mb-2">Explore:</h3>
         <div className="grid grid-cols-4 gap-1 ">
-          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded ">
+          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded cursor-pointer">
             <Link
               to="/hackathons"
               style={{ textDecoration: "none", color: "inherit" }}
@@ -17,7 +17,7 @@ const Home = () => {
               Compete in Hackathons
             </Link>
           </div>
-          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded ">
+          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded cursor-pointer">
             <Link
               to="/scholarships"
               style={{ textDecoration: "none", color: "inherit" }}
@@ -25,16 +25,19 @@ const Home = () => {
               Apply for Scholarhips
             </Link>
           </div>
-          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded ">
+          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded cursor-pointer">
             <Link
               to="/internships"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              Internships
+              Looking for Internships
             </Link>
           </div>
-          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded ">
-            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <div className=" border-2 border-blue-200 hover:bg-blue-400 text-black hover:text-white px-4 py-2 rounded cursor-pointer">
+            <Link
+              to="/resume-bulderLink"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
               Create Resume
             </Link>
           </div>
@@ -42,9 +45,11 @@ const Home = () => {
 
         {/* latest opportunites section */}
         <div>
-          <div className="my-5"><h3 className="text-xl">Latest and upcomming HACKATHONS</h3></div>
-          <div className="flex flex-wrap ">
-            <div className="w-1/3 mb-3">
+          <div className="my-5">
+            <h3 className="text-xl">Latest and upcomming HACKATHONS</h3>
+          </div>
+          <div className="flex justify-between mb-3">
+            <div className="">
               <LatestFeed
                 titleType="Hackathon"
                 title="1Password Hackathon"
@@ -53,14 +58,14 @@ const Home = () => {
                 coverImg="https://blog.1password.com/posts/2023/2023-1password-hashnode-hackathon/header.png"
               />
             </div>
-            <div className="w-1/3 mb-3">
+            <div className="">
               <LatestFeed
                 registerLink="https://hashnode.com/hackathons/appwrite"
                 coverImg="https://cdn.hashnode.com/res/hashnode/image/upload/v1683863785104/ZxY0JVG0R.png?auto=format"
               />
             </div>
 
-            <div className="w-1/3 mb-3">
+            <div className="">
               <LatestFeed
                 registerLink="https://devfolio.co/hackathons"
                 coverImg="https://devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fcontent%2Fa1f504bee74b4f19be305d409aa4fc16%2F630b3009-0ad9-407c-acc8-7eec7c928347.png&w=1440&q=75"
@@ -73,8 +78,8 @@ const Home = () => {
           <div className="my-5">
             <h2 className="text-xl">Latest INTERNSHIP opportunites </h2>
           </div>
-          <div className="flex flex-wrap ">
-            <div className="w-1/3 mb-3">
+          <div className="flex justify-between ">
+            <div>
               <LatestFeed
                 titleType="Hackathon"
                 title="1Password Hackathon"
@@ -83,21 +88,18 @@ const Home = () => {
                 coverImg="https://blog.1password.com/posts/2023/2023-1password-hashnode-hackathon/header.png"
               />
             </div>
-            <div className="w-1/3 mb-3">
+            <div>
               <LatestFeed
                 registerLink="https://hashnode.com/hackathons/appwrite"
                 coverImg="https://cdn.hashnode.com/res/hashnode/image/upload/v1683863785104/ZxY0JVG0R.png?auto=format"
               />
             </div>
 
-            <div className="w-1/3 mb-3">
+            <div>
               <LatestFeed
                 registerLink="https://devfolio.co/hackathons"
                 coverImg="https://devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fcontent%2Fa1f504bee74b4f19be305d409aa4fc16%2F630b3009-0ad9-407c-acc8-7eec7c928347.png&w=1440&q=75"
               />
-            </div>
-            <div className="w-1/3 mb-3">
-              <LatestFeed registerLink="https://1password.com" />
             </div>
           </div>
         </div>
